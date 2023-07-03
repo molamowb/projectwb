965f8e68b3e9ee95d80f7d8776d6db944035eda9
molamowb <106499447+molamowb@users.noreply.github.com>
2023-07-03 22:42:15 +0800

    Update trans_bus_ctb_nwfb.py

--- trans_bus_ctb_nwfb.py
+++ trans_bus_ctb_nwfb.py
@@ -128,5 +128,5 @@ def getRouteStop(co):
     with open(STOP_LIST, 'w') as f:
         f.write(json.dumps(stopList, ensure_ascii=False))
 
-getRouteStop('nwfb')
+#getRouteStop('nwfb')
 getRouteStop('ctb')
