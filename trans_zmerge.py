f6301e0cbfe8714e3b51a6a2ac21b603b42c253c
molamowb <106499447+molamowb@users.noreply.github.com>
2023-07-03 22:42:49 +0800

    Update trans_zmerge.py

--- trans_zmerge.py
+++ trans_zmerge.py
@@ -81,7 +81,7 @@ def importRouteListJson( co ):
   
 importRouteListJson('kmb')
 importRouteListJson('ctb') 
-importRouteListJson('nwfb')
+#importRouteListJson('nwfb')
 importRouteListJson('gmb')
 importRouteListJson('nlb')
 importRouteListJson('lr')
